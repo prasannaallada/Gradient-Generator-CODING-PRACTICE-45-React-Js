0a7624dea2d7f91dfc1c8b738308d020d2c24b4d
prasannaallada <prasannaallada9@gmail.com>
2023-04-29 05:51:33 +0000

    gradient generator

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// published url: pragradi.ccbp.tech
+
+import GradientGenerator from './components/GradientGenerator'
+
+import './App.css'
+
+const App = () => <GradientGenerator />
+
+export default App
